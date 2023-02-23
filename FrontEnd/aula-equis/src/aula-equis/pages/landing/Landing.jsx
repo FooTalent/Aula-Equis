87bfbcabdaceef6583646caf7904e82986adbd05
luisnava6667 <navaluisrodolfo@gmail.com>
2023-02-23 19:20:47 -0300

    create LandingPage

--- FrontEnd/aula-equis/src/aula-equis/pages/landing/Landing.jsx
+++ FrontEnd/aula-equis/src/aula-equis/pages/landing/Landing.jsx
@@ -1,10 +1,9 @@
-import { Navbar, Button } from '../../components'
-const Landing = () => {
+export const Landing = () => {
   return (
-    <>
-
-    </>
+    <div
+      className="bg-gradient-to-br from-amber-500 via-amber-100 to-white
+      h-screen">
+      <h1>landung</h1>
+    </div>
   )
 }
-
-export default Landing
