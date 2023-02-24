a198469c919f155e0846018084a5630cf04f367c
Gonzalo Rodriguez <gonzalo-ezequiel@hotmail.com>
2023-02-24 10:42:23 -0300

    add NavBar Component

--- FrontEnd/aula-equis/src/aula-equis/pages/landing/Landing.jsx
+++ FrontEnd/aula-equis/src/aula-equis/pages/landing/Landing.jsx
@@ -1,9 +1,15 @@
+import Navbar from '../../components/NavBar/Navbar'
+
 export const Landing = () => {
   return (
-    <div
-      className="bg-gradient-to-br from-amber-500 via-amber-100 to-white
-      h-screen">
-      <h1>landung</h1>
-    </div>
+    <>
+      <div
+        className="bg-gradient-to-br from-amber-500 via-amber-100 to-white
+      h-screen"
+      >
+        <Navbar />
+        <h1>landung</h1>
+      </div>
+    </>
   )
 }
