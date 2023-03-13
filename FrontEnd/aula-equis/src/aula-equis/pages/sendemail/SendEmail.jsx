477dbc9c966cea9377e5bbca4662eb9c7229fbf8
Gonzalo Rodriguez <gonzalo-ezequiel@hotmail.com>
2023-03-13 20:16:56 -0300

    add_PageSendEmail

--- FrontEnd/aula-equis/src/aula-equis/pages/sendemail/SendEmail.jsx
+++ FrontEnd/aula-equis/src/aula-equis/pages/sendemail/SendEmail.jsx
@@ -7,35 +7,18 @@ import axios from 'axios'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
-const FormLogin = () => {
-  const [hidden, setHidden] = useState('password')
-  const [imgPass, setImgPass] = useState(
-    <IoIosLock className="h-4 text-purple-700" />
-  )
-
+const SendEmail = () => {
   const userRol = consultarPath()
   console.log(userRol)
-  const typeInput = () => {
-    if (hidden === 'password') {
-      setHidden('text')
-      setImgPass(<IoIosUnlock className="h-4 text-purple-700" />)
-    } else {
-      setHidden('password')
-      setImgPass(<IoIosLock className="h-4 text-purple-700" />)
-    }
-  }
+
   const formik = useFormik({
     initialValues: {
       email: '',
-      password: '',
     },
     validationSchema: Yup.object({
       email: Yup.string()
         .email('El correo electrónico es inválido')
         .required('El correo electrónico es requerido'),
-      password: Yup.string()
-        .required('La contraseña es requerida')
-        .min(3, 'La contraseña debe tener al menos 3 caracteres'),
     }),
     onSubmit: (values) => {
       console.log(values)
@@ -61,7 +44,6 @@ const FormLogin = () => {
     }
   }
   const { handleSubmit, handleChange, handleBlur, touched, errors } = formik
-
   return (
     <>
       <div className={`bg-gradient-to-b ${userRol.bg}`}>
@@ -93,7 +75,7 @@ const FormLogin = () => {
         </header>
         <div className="mx-auto max-w-screen-xl  pl-0 pr-4 md:pl-0 sm:pr-6 sm:pl-0 xl:ml-0 xl:max-w-full ">
           {
-            <div className="hidden md:flex md:justify-end md:mr-[15%] md:mb-20 md:mt-36 xl:mb-4 xl:mt-24 ">
+            <div className="hidden md:flex md:justify-end md:mr-[15%] md:mb-20 md:mt-36 lg:mb-0 xl:mb-4 xl:mt-24 ">
               <nav className="flex items-center justify-center font-medium">
                 <a className="text-white text-4xl md:text-[64px]" href="">
                   {userRol.rol}
@@ -101,8 +83,9 @@ const FormLogin = () => {
               </nav>
             </div>
           }
+
           <div className="grid grid-cols-1 gap-x-16 md:gap-x-0 gap-y-8 md:flex md:items-center md:justify-between lg:grid-cols-5">
-            <div className="flex gap-[153px] lg:col-span-2 lg:py-12 sm:mt-16">
+            <div className="flex gap-[30px] sm:gap-[120px] lg:col-span-2 lg:py-0 sm:mt-16">
               <img
                 className="w-40 h-48 md:w-imgLoginMd md:h-imgLoginMd lg:w-imgLoginLg lg:h-imgLoginLg"
                 src={userRol.img}
@@ -115,15 +98,19 @@ const FormLogin = () => {
             </div>
 
             <div
-              className={`flex justify-center sm:rounded-lg md:mr-20 xl:mr-4 p-8 md:shadow-lg md:w-formLoginMd md:h-formLoginMd xl:w-[610px] lg:col-span-3 lg:p-12  md:bg-gradient-to-b ${userRol.bg}`} /* className="mx-auto sm:px-6" */
+              className={`flex justify-center sm:rounded-lg  xl:mr-4 p-8 md:shadow-lg md:w-formLoginMd md:h-formLoginMd xl:w-[610px] lg:col-span-3 lg:p-12  md:bg-gradient-to-b ${userRol.bg}`}
             >
               <form
                 onKeyDown={handleKeyDown}
                 onSubmit={handleSubmit}
                 className=" md:h-[26.5rem]"
               >
-                <p className="hidden md:flex md:justify-center text-center text-white text-2xl font-bold ">
-                  INICIAR SESIÓN
+                <p className="hidden md:flex md:justify-center text-center text-sm text-white lg:text-2xl font-bold ">
+                  RESTABLECER CONTRASEÑA
+                </p>
+                <p className="hidden md:flex md:justify-center text-center text-xs text-white lg:text-lg font-bold ">
+                  Ingresa tu correo electrónico y te enviaremos un mail para
+                  restablecer tu contraseña
                 </p>
                 <div className="grid grid-cols-1 gap-7 ">
                   <div className="">
@@ -150,46 +137,24 @@ const FormLogin = () => {
                       </div>
                     )}
                   </div>
-                  <div className="">
-                    <div className="">
-                      <label htmlFor="password" className="text-[#FBFBFB] mb-2">
-                        Contraseña:
-                      </label>
-                      <input
-                        type={hidden}
-                        className="h-16 rounded-2xl border-2 border-gray-600  pr-12 shadow-sm w-full p-3 text-sm"
-                        placeholder="Contraseña"
-                        name="password"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                    </div>
-                    <div
-                      onClick={typeInput}
-                      className="grid justify-end cursor-pointer -mt-9 mr-3"
-                    >
-                      {imgPass}
-                    </div>
-                    {touched.password && errors.password && (
-                      <div className="flex flex-row-reverse w-[15.5rem] sm:w-[19.5rem] text-red-500 mt-5 text-xs sm:text-sm">
-                        {errors.password}
-                        <IoIosAlert />
-                      </div>
-                    )}
-                  </div>
                 </div>
                 <div className="mt-2">
                   <input
                     type="submit"
-                    className="h-14 rounded-2xl  bg-yellow-500 text-sm cursor-pointer inline-block w-full mt-6  px-5 py-3 font-medium text-white"
-                    value="Iniciar sesión"
+                    className="h-14 rounded-2xl  bg-yellow-500 text-sm cursor-pointer inline-block w-full mt-6  px-5 py-3 font-medium text-white lg:text-2xl"
+                    value="Restablecer"
                   />
                 </div>
                 <div className="relative flex justify-center mt-2">
-                  <p className="text-xs font-normal text-center" href="">
-                    ¿Olvidaste tu contraseña?{' '}
-                    <Link className="underline text-purple-700">
-                      Restablecer constraseña
+                  <p
+                    className="text-xs font-normal text-center lg:text-xl"
+                    href=""
+                  >
+                    <Link
+                      to={`/login-${userRol.rol.toLowerCase()}`}
+                      className="underline text-purple-700"
+                    >
+                      Cancelar
                     </Link>
                   </p>
                 </div>
@@ -214,4 +179,4 @@ const FormLogin = () => {
   )
 }
 
-export default FormLogin
+export default SendEmail
--- FrontEnd/aula-equis/src/router/AppRouter.jsx
+++ FrontEnd/aula-equis/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import FormRegister from '../aula-equis/components/FormRegister/FormRegister'
 import { Home } from '../aula-equis/components/Home/Home'
 import FormLogin from '../aula-equis/pages/formLogin/FormLogin'
 import { Landing } from '../aula-equis/pages/landing/Landing'
+import SendEmail from '../aula-equis/pages/sendemail/SendEmail'
 
 export const AppRouter = () => {
   return (
@@ -13,6 +14,10 @@ export const AppRouter = () => {
       <Route path="/login-tutor" element={<FormLogin />} />
       <Route path="/login-escuela" element={<FormLogin />} />
       <Route path="/login-register" element={<FormRegister />} />
+      <Route path="/login-send-email-alumno" element={<SendEmail />} />
+      <Route path="/login-send-email-docente" element={<SendEmail />} />
+      <Route path="/login-send-email-tutor" element={<SendEmail />} />
+      <Route path="/login-send-email-escuela" element={<SendEmail />} />
       <Route path="/form-register" element={<FormRegister />} />
       <Route path="/home" element={<Home />} />
     </Routes>
