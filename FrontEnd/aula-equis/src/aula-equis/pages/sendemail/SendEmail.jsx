1935424a756f1abaca8c83a1c03a7d35aa22ecae
Gonzalo Rodriguez <109924354+gonzaloerodriguez@users.noreply.github.com>
2023-03-13 23:32:02 -0300

    Rl send email for recovery password (#15)
    * add_PageSendEmail
    
    * fixing responsive in SendEmail and FormLogin

--- FrontEnd/aula-equis/src/aula-equis/pages/sendemail/SendEmail.jsx
+++ FrontEnd/aula-equis/src/aula-equis/pages/sendemail/SendEmail.jsx
@@ -7,35 +7,18 @@ import axios from 'axios'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
-const FormLogin = () => {
-  const [hidden, setHidden] = useState('password')
-  const [imgPass, setImgPass] = useState(
-    <IoIosLock className="h-4 text-purple-700" />
-  )
-
+const SendEmail = () => {
   const userRol = consultarPath()
   console.log(userRol)
-  const typeInput = () => {
-    if (hidden === 'password') {
-      setHidden('text')
-      setImgPass(<IoIosUnlock className="h-4 text-purple-700" />)
-    } else {
-      setHidden('password')
-      setImgPass(<IoIosLock className="h-4 text-purple-700" />)
-    }
-  }
+
   const formik = useFormik({
     initialValues: {
       email: '',
-      password: '',
     },
     validationSchema: Yup.object({
       email: Yup.string()
         .email('El correo electrónico es inválido')
         .required('El correo electrónico es requerido'),
-      password: Yup.string()
-        .required('La contraseña es requerida')
-        .min(3, 'La contraseña debe tener al menos 3 caracteres'),
     }),
     onSubmit: (values) => {
       console.log(values)
@@ -61,11 +44,6 @@ const FormLogin = () => {
     }
   }
   const { handleSubmit, handleChange, handleBlur, touched, errors } = formik
-  const [modalTC, setOpenModalTC] = useState(false)
-  const openModal = () => {
-    setOpenModalTC(!modalTC)
-  }
-
   return (
     <>
       <div className={`bg-gradient-to-b ${userRol.bg}`}>
@@ -127,8 +105,12 @@ const FormLogin = () => {
                 onSubmit={handleSubmit}
                 className=" md:h-[26.5rem]"
               >
-                <p className="hidden md:flex md:justify-center text-center text-white text-2xl font-bold ">
-                  INICIAR SESIÓN
+                <p className="hidden md:flex md:justify-center text-center text-sm text-white lg:text-2xl font-bold ">
+                  RESTABLECER CONTRASEÑA
+                </p>
+                <p className="hidden md:flex md:justify-center text-center text-xs text-white lg:text-lg font-bold ">
+                  Ingresa tu correo electrónico y te enviaremos un mail para
+                  restablecer tu contraseña
                 </p>
                 <div className="grid grid-cols-1 gap-7 ">
                   <div className="">
@@ -155,78 +137,40 @@ const FormLogin = () => {
                       </div>
                     )}
                   </div>
-
-                  <div className="">
-                    <div className="">
-                      <label htmlFor="password" className="text-[#FBFBFB] mb-2">
-                        Contraseña:
-                      </label>
-                      <input
-                        type={hidden}
-                        className="h-16 rounded-2xl border-2 border-gray-600  pr-12 shadow-sm w-full p-3 text-sm"
-                        placeholder="Contraseña"
-                        name="password"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                      />
-                    </div>
-                    <div
-                      onClick={typeInput}
-                      className="grid justify-end cursor-pointer -mt-9 mr-3"
-                    >
-                      {imgPass}
-                    </div>
-                    {touched.password && errors.password && (
-                      <div className="flex flex-row-reverse w-[15.5rem] sm:w-[19.5rem] text-red-500 mt-5 text-xs sm:text-sm">
-                        {errors.password}
-                        <IoIosAlert />
-                      </div>
-                    )}
-                  </div>
                 </div>
                 <div className="mt-2">
                   <input
                     type="submit"
-                    className="h-14 rounded-2xl  bg-yellow-500 text-sm cursor-pointer inline-block w-full mt-6  px-5 py-3 font-medium text-white"
-                    value="Iniciar sesión"
+                    className="h-14 rounded-2xl  bg-yellow-500 text-sm cursor-pointer inline-block w-full mt-6  px-5 py-3 font-medium text-white lg:text-2xl"
+                    value="Restablecer"
                   />
                 </div>
                 <div className="relative flex justify-center mt-2">
-                  <p className="text-xs font-normal text-center" href="">
-                    ¿Olvidaste tu contraseña?{' '}
-                    <Link className="underline text-purple-700">
-                      Restablecer constraseña
+                  <p
+                    className="text-xs font-normal text-center lg:text-xl"
+                    href=""
+                  >
+                    <Link
+                      to={`/login-${userRol.rol.toLowerCase()}`}
+                      className="underline text-purple-700"
+                    >
+                      Cancelar
                     </Link>
                   </p>
                 </div>
                 <div className="flex justify-center mt-1">
                   <p className="flex flex-col md:block text-xs font-normal text-center">
                     Al Iniciar Sesión, estas aceptando los{' '}
-                    <Link
-                      onClick={openModal}
-                      className="underline text-purple-700"
-                    >
+                    <Link className="underline text-purple-700">
                       Términos y condiciones
                     </Link>{' '}
                     y nuestras políticas sobre{' '}
-                    <Link
-                      onClick={openModal}
-                      className="underline text-purple-700"
-                    >
+                    <Link className="underline text-purple-700">
                       Protección de Datos.
                     </Link>
                   </p>
                 </div>
               </form>
-                <div className={` ${modalTC ? ' w-full h-full bg-white fixed left-0 top-0 bottom-0 right-0 flex flex-col justify-center items-center' : ' hidden'}`}>
-                  <p className=' font-bold text-2xl w-96'>
-                    Términos y condiciones de uso y proteccion de datos(parte
-                    legal)
-                  </p>
-                  <button onClick={openModal} className='mt-5 bg-orange-500 text-white w-96 rounded-xl p-2'>
-                    Volver
-                  </button>
-                </div>
             </div>
           </div>
         </div>
@@ -235,4 +179,4 @@ const FormLogin = () => {
   )
 }
 
-export default FormLogin
+export default SendEmail
