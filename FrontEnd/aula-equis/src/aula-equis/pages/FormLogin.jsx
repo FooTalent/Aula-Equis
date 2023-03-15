1e5f580a5ac883058e1a7f9a76b5e815c89e7b3d
luisnava6667 <navaluisrodolfo@gmail.com>
2023-03-15 15:53:05 -0300

    ft_Home

--- FrontEnd/aula-equis/src/aula-equis/pages/FormLogin.jsx
+++ FrontEnd/aula-equis/src/aula-equis/pages/FormLogin.jsx
@@ -1,4 +1,4 @@
-import { IoIosAlert, IoIosLock, IoIosUnlock, IoMdMail } from 'react-icons/Io'
+import { IoIosAlert, IoIosLock, IoIosUnlock, IoMdMail } from 'react-icons/io'
 import logo from '../components/assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
