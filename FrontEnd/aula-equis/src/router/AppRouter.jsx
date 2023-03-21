f7c552763169fe5b7a17ebd1595e89bde65c8180
Leandromarelli <108134019+Leandromarelli@users.noreply.github.com>
2023-03-21 20:42:45 -0300

    Ft form register leandro (#25)
    * create LandingPage
    
    * add NavBar Component
    
    * addImageUrlNavBar
    
    * resposive LandingPage
    
    * modified navbar
    
    * modified NavBar
    
    * FormRegister - faltan los estilos
    
    * Formulario - falta algo de edicion
    
    * ft_landing
    
    * ft_landing_Resposive
    
    * ft_landing_Resposive
    
    * faltan estilos al formulario
    
    * Finalizado el formulario FormRegister
    
    * Fin - chequear estética
    
    * Haciendo validaciones
    
    * validacion ok
    
    * haciendo el responsive
    
    * Responsive ok
    
    ---------
    
    Co-authored-by: luisnava6667 <navaluisrodolfo@gmail.com>
    Co-authored-by: Gonzalo Rodriguez <gonzalo-ezequiel@hotmail.com>

--- FrontEnd/aula-equis/src/router/AppRouter.jsx
+++ FrontEnd/aula-equis/src/router/AppRouter.jsx
@@ -1,1 +1,23 @@
 
+import { Route, Routes } from 'react-router-dom'
+import FormRegister from '../aula-equis/components/FormRegister/FormRegister'
+import { Home } from '../aula-equis/components/Home/Home'
+import FormLogin from '../aula-equis/pages/formLogin/FormLogin'
+import { Landing } from '../aula-equis/pages/landing/Landing'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/lan" element={<Landing />} />
+      <Route path="/login-alumno" element={<FormLogin />} />
+      <Route path="/login-docente" element={<FormLogin />} />
+      <Route path="/login-tutor" element={<FormLogin />} />
+      <Route path="/login-escuela" element={<FormLogin />} />
+      <Route path="/login-register" element={<FormRegister />} />
+      <Route path="/form-register" element={<FormRegister />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )
+}
+
+
